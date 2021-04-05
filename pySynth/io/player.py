6674b8e76f07a43d31bf47f2dd209d9b7c85d97c
yc2288 <yc2288@cornell.edu>
2021-04-05 18:08:23 -0400

    def player

---
+++ pySynth/io/player.py
@@ -0,0 +1,13 @@
+import simpleaudio as sa
+
+
+class Player:
+
+    def __init__(self):
+        pass
+
+    def play(self, signal):
+        pass
+
+    def __call__(self, signal):
+        self.play(signal)
