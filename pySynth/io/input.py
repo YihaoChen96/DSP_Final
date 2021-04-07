010c3469a5096af4d3fed915bc78fab75f37e232
yc2288 <yc2288@cornell.edu>
2021-04-07 12:57:29 -0400

    start IO implementation

--- pySynth/io/input.py
+++
@@ -1,2 +0,0 @@
-class InputProcessor:
-    def __
---
+++ pySynth/io/output.py
@@ -0,0 +1,8 @@
+from scipy.io import wavfile
+
+
+def loadMidi(midi_filename):
+    raise NotImplementedError
+
+def loadConfig(config_filename):
+    raise NotImplementedError
