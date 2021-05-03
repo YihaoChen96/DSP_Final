27d2f161d33bcac213af91e977acc4e8feda29c0
yc2288 <yc2288@cornell.edu>
2021-05-03 19:50:45 -0400

    add midi stream

--- pySynth/synth/synthesizer.py
+++ pySynth/synth/synthesizer.py
@@ -3,7 +3,7 @@ import numpy as np
 from .oscillator import OSC
 from .filter import BandPass, LowPass
 from .effect import Chorus
-from pySynth.control.stream import Stream
+from pySynth.control.stream import Stream, MIDIStream
 class AddSynth:
     def __init__(self, *streams):
         self.streams = streams
