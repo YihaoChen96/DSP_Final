e99cf27f190c4c6c895924f201e1ace134d45498
yc2288 <yc2288@cornell.edu>
2021-05-03 19:34:30 -0400

    add midi stream and chain

--- pySynth/control/stream.py
+++ pySynth/control/stream.py
@@ -3,28 +3,51 @@ from pySynth.synth.filter import *
 from pySynth.synth.oscillator import *
 from pySynth.synth.synthesizer import *
 
+def Midi2Freq(self, pitch, fA4InHz = 440):
+    if fA4InHz <= 0:
+        return 0
+    else:
+        return fA4InHz * 2**((pitch-69) / 12)
 
 class Chain:
-    def __init__(self, module_list):
+    def __init__(self, generator, effects):
+        self.generator = generator
+        self.effects = effects
 
-        self.module_list = module_list
+    def __call__(self):
 
-    def generate(self):
+        assert isinstance(self.generator, OSC), "The first module must be an oscillator"
 
-        assert isinstance(self.module_list[0], OSC), "The first module must be an oscillator"
+        for effect in self.effects:
+            assert not isinstance(effect, OSC), "Oscillator cannot be in effect chain"
 
-        for i in module_list:
-            assert not isinstance(self.module_list[i], OSC), "Oscillator must be the first module"
-
-        signal = module_list[0]()
-        for i in range(1, len(module_list)):
-            signal = module_list[i](signal)
+        signal = self.generator()
+        for i in range(len(self.effects)):
+            signal = self.effects[i](signal)
 
         return signal
 
-class Stream:
-    def __init__(self, input, chain):
-        pass
+class MIDIStream:
+    def __init__(self, osc_type, effects_chain):
+        self.osc_type = osc_type
+        self.effects = effects_chain
 
-    def __call__(self):
-        pass
+    def __call__(self, midi_info_ls):
+        if len(midi_info_ls) == 0:
+            raise RuntimeError("Empty Midi List")
+
+        output_stream = []
+
+        prev_ls = [0,0,0]
+        for ls in midi_info_ls: # for each note event
+            [pitch, start, end] = ls
+            if start - prev_ls[2] > 0: # having rest between previous event
+                osc = OSC(self.osc_type,  2, start-prev_ls[2])
+                chain = Chain(osc, self.effects)
+                output_stream.append(osc)
+            freq = self.ToolMidi2Freq(pitch)
+            osc = OSC(self.osc_type , freq, end-start)
+            self.output_osc_array.extend(osc())
+            prev_ls = ls
+
+        return self.output_chains
