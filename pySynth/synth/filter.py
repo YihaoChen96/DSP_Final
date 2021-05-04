f6001a51a7e4d82bace778dbddfc059c0f59c9ce
yc2288 <yc2288@cornell.edu>
2021-05-03 20:32:51 -0400

    change input placement and naming

--- pySynth/synth/filter.py
+++ pySynth/synth/filter.py
@@ -3,22 +3,22 @@ import numpy as np
 from scipy import signal
 
 class BandPass:
-    def __init__(self,signal,Low_Band=20,High_Band=1000,number_of_degree=1):
+    def __init__(self, Low_Band=20,High_Band=1000,number_of_degree=1):
         self.sr=44100
         self.Low=Low_Band
         self.High=High_Band
         self.Degree=number_of_degree
-        self.Signal=signal
-    def __call__(self):
+        
+    def __call__(self, input):
         b, a = signal.butter(self.Degree, [2.0*self.Low/self.sr,2.0*self.High/self.sr], 'bandpass')
-        return signal.filtfilt(b,a,self.signal)
+        return signal.filtfilt(b,a,input)
 
 class LowPass:
-    def __init__(self,signal,Low_Band=20,number_of_degree=1):
+    def __init__(self,Low_Band=20,number_of_degree=1):
         self.sr=44100
         self.Low=Low_Band
         self.Degree=number_of_degree
-        self.Signal=signal
-    def __call__(self):
+        
+    def __call__(self, input):
         b, a = signal.butter(self.Degree, 2.0*self.Low/self.sr, 'lowpass')
-        return signal.filtfilt(b,a,self.signal)
+        return signal.filtfilt(b,a,input)
