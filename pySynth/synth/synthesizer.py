460e918d30b0bebf44ea69e63cd513cc9665e8f8
yc2288 <yc2288@cornell.edu>
2021-05-03 20:42:37 -0400

    padd inputs

--- pySynth/synth/synthesizer.py
+++ pySynth/synth/synthesizer.py
@@ -6,12 +6,14 @@ from .effect import Chorus
 from pySynth.control.stream import Stream, MIDIStream
 class AddSynth:
     def __init__(self, *streams):
-        self.streams = streams
-        # for stream in streams:
-        #     assert isinstance(stream, OSC) or isinstance(stream, Stream)
+        self.streams = list(streams)
+        self.max_len = max([len(s) for s in self.streams])
+
+        for i in range(len(self.streams)):
+            self.streams[i] = np.pad(self.streams[i], (0, self.max_len - len(self.streams[i])))
+
     
     def __call__(self):
-        # return np.mean([stream() for stream in self.streams], axis = 0)
         return np.mean(self.streams, axis = 0)
 
 
