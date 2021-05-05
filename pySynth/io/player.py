dbc73d76191538e26a1395edd67e1a6424fe4bb7
yc2288 <yc2288@cornell.edu>
2021-05-04 21:25:54 -0400

    simpified player

--- pySynth/io/player.py
+++ pySynth/io/player.py
@@ -3,36 +3,20 @@ import numpy as np
 
 class Player:
 
-    def __init__(self, sr = 44100, bit_depth = 16):
+    def __init__(self, sr = 44100):
         
         self.sr = sr
-        self.bit_depth = 16
-
-    def play(self, signal, sr = None, bit_depth = None):
         
-        """
-            TODO: Different bit range
-            TODO: Multi-channel
-            Curtesy Code example from: https://realpython.com/playing-and-recording-sound-python/
 
-            params: 
-                signal: a list or numpy array containing the raw signal output
-        """
+    def play(self, signal, sr = None):
+        
 
         if sr is None:
             sr = self.sr
+
+        audio = signal * (2**(16-1) - 1) / np.max(np.abs(signal))
         
-        if bit_depth is None:
-            bit_depth = self.bit_depth
-
-
-        # Ensure that highest value is in bit range
-        audio = signal * (2**(bit_depth-1) - 1) / np.max(np.abs(signal))
-        
-
-        # Convert to bit_range data
-        if bit_depth == 16:
-            dtype = np.int16
+        dtype = np.int16
             
         audio = audio.astype(dtype)
 
