8f00f3348a1a6e214417113f65277b2038c1a661
yc2288 <yc2288@cornell.edu>
2021-05-04 21:06:01 -0400

    remove unused packages

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name='pySynth', 
     version='0.1', 
     packages=find_packages(),
-    install_requires = ["numpy", "scipy", "simpleaudio", "mido", "pydub", "pretty-midi", "SoundFile"]
+    install_requires = ["numpy", "scipy", "simpleaudio", "pretty-midi", "SoundFile"]
     )
